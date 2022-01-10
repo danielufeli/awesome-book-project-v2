144dcea28794c6c060582df30edb00c035284ad9
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-01-10 17:28:05 -0500

    Change click listener to submit and fix navigation

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import Book from './modules/Book.js';
-const submit = document.getElementById('btn-add');
 
-submit.addEventListener('click', () => {
+const submit = document.querySelector('form');
+
+submit.addEventListener('submit', (e) => {
+  e.preventDefault();
   Book.addBookMethod();
 });
 
 Book.displayBooks();
+
+const list = document.getElementById('list-link');
+const add = document.getElementById('add-link');
+const contact = document.getElementById('contact-link');
+
+list.addEventListener('click', () => {
+  Book.listSection();
+});
+add.addEventListener('click', () => {
+  Book.bookForm();
+});
+contact.addEventListener('click', () => {
+  Book.contactSection();
+});
